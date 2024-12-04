83e891ef72ec6942ffd589405a040f7a78074cdf
macostacos <ivanov.maxim0@yandex.ru>
2024-12-04 22:05:18 +0300

    calculated

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,31 +1,44 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    private static Calculator calculator;
+
+    @BeforeAll
+    static void setup() {
+        calculator = new Calculator();
+    }
+
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        Assertions.assertEquals(15, calculator.add(10, 5));
+        Assertions.assertEquals(51, calculator.add(10, 5));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        Assertions.assertEquals(15, calculator.dif(20, 5));
+        Assertions.assertEquals(51, calculator.dif(20, 5));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        Assertions.assertEquals(15, calculator.div(45, 3));
+        Assertions.assertEquals(51, calculator.div(45, 3));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        Assertions.assertEquals(15, calculator.times(5, 3));
+        Assertions.assertEquals(51, calculator.times(5, 3));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        Assertions.assertEquals(18 , calculator.solver());
     }
 }
