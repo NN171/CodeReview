97bacef5c0f0183e0b1e68152d937f6e88987569
macostacos <ivanov.maxim0@yandex.ru>
2024-12-04 22:17:59 +0300

    style fixes

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,10 +1,13 @@
 public class Calculator {
+
     public int add(int a, int b){
         return a + b;
     }
+
     public int dif(int a, int b){
         return a - b;
     }
+
     public int div(int a, int b){
         if (b == 0) {
             throw new IllegalArgumentException();
@@ -12,9 +15,11 @@ public class Calculator {
             return a / b;
         }
     }
+
     public int times(int a, int b){
         return a * b;
     }
+
     public int solver(){
         return times(dif(div(8, 2), 1) , add(1, 5));
     }
