d86d6031a0ac9101428555928757fdaaf70f0bfd
nn17 <yakor356@gmail.com>
2024-12-04 22:03:59 +0300

    add tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,31 +1,46 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    private static Calculator calculator;
+
+    @BeforeAll
+    static void setup() {
+        calculator = new Calculator();
+    }
+
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        Assertions.assertEquals(5, calculator.add(3, 2)); //Success
+        Assertions.assertNotEquals(6, calculator.add(2, 1)); //Fail
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        Assertions.assertEquals(6, calculator.dif(8, 2)); //Success
+        Assertions.assertNotEquals(6, calculator.dif(7, 5)); //Fail
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        Assertions.assertEquals(3, calculator.div(6, 2)); //Success
+        Assertions.assertNotEquals(2, calculator.div(4, 1)); //Fail
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        Assertions.assertEquals(6, calculator.times(2, 3)); //Success
+        Assertions.assertNotEquals(8, calculator.times(1, 3)); //Fail
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        Assertions.assertEquals(24, calculator.solver()); //Success
+        Assertions.assertNotEquals(15, calculator.solver()); //Fail
     }
 }
